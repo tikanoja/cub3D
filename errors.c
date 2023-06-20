490aaf02acdf91fbd3645ac2a5eefe6509a3b11d
Joonatan Aurasmaa <jaurasma@c2r3p13.hive.fi>
2023-06-20 16:06:29 +0300

    no leaks

--- errors.c
+++ errors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttikanoj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jaurasma <jaurasma@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 10:03:46 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/06/20 10:03:47 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/06/20 16:05:48 by jaurasma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ int	exit_gracefully(t_master *master)
 	free_char_arr(master->data.map);
 	free_char_arr(master->data.wall);
 	free_textures(master, 4);
-	mlx_destroy_image(master->mlx.mlx_ptr, master->img.img);
 	mlx_destroy_window(master->mlx.mlx_ptr, master->mlx.mlx_win);
 	free(master->mlx.mlx_ptr);
 	ft_putstr_fd("Thanks for playing :)\n", 1);
